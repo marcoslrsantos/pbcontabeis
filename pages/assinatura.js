d86a21488a2c08140248636b91b5e3f71372d629
mlrsantos <lopes.marcos@yahoo.com.br>
2022-01-17 17:25:23 -0300

    Insert signature
    Inserido assinatura

---
+++ pages/assinatura.js
@@ -0,0 +1,7 @@
+function Assinatura(){
+    return <div>
+        <img src="./logo_email_50.jpg" />
+    </div>
+}
+
+export default Assinatura
---
+++ pages/assinatura2.js
@@ -0,0 +1,7 @@
+function Assinatura2(){
+    return <div>
+        <img src="./logo_sem-borda.jpg" />
+    </div>
+}
+
+export default Assinatura2
